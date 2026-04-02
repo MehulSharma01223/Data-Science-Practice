d7c56a4895b7617b721ac5a4a873093c0b139063
MehulSharma01223 <sharmamehul4649@gmail.com>
2026-04-02 17:20:28 +0530

    Add second questions in medium.py

--- numpy/medium.py
+++ numpy/medium.py
@@ -1,6 +1,6 @@
 # Day 1
 
-# Q11: Remove duplicates
+# Q1: Remove duplicates
 arr = [1,2,2,3,4,4]
 unique = []
 
@@ -9,3 +9,20 @@ for i in arr:
         unique.append(i)
 
 print(unique)
+
+# Q2:  (1) Common elements
+#      (2) Elements presents in A not in B
+
+A = [1,2,3,4,5]
+B = [3,4,5,6,7]
+common = []
+new = []
+for i in  A:
+    if i in B:
+        if i not in common :
+            common.append(i)
+        else :
+            new.append(i)
+print("common:",common)
+print("A not in B:",new)
+            
