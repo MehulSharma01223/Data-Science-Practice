aedddce0ffa6f8547f2e7cf95128b470c3280016
MehulSharma01223 <sharmamehul4649@gmail.com>
2026-04-06 17:26:48 +0530

    Add solutions for multiple NumPy array manipulation problems

--- numpy/easy.py
+++ numpy/easy.py
@@ -105,3 +105,104 @@ A = np.array([1,2,3,4,5])
 new = A **2
 print(new)
 
+
+
+# =========================
+# Day 4
+# =========================
+
+#  Q7.   Create array [3,6,9,12,15]
+#        Find difference between first and last element
+#        Find middle element
+
+
+
+import numpy as np
+
+A = np.array([3,6,9,12,15])
+
+# Difference
+b = A[-1] - A[0]
+
+# Middle element
+middle = A[len(A)//2]
+
+print("Difference:", b)
+print("Middle element:", middle)
+
+
+# Q8.   Create array [2,4,6,8]
+#       Insert value 10 at the end
+#       Insert value 0 at the beginning
+import numpy as np
+a = np.array([2,4,6,8])
+a[0] = 0
+a[len(a)-1] = 10
+print(a)
+
+
+# Q9.   Create array [1,2,3,4,5]
+#       Remove element 3
+#       Print updated array
+
+import numpy as np
+
+a = np.array([2,4,6,8])
+
+# Insert 10 at end
+a = np.insert(a, len(a), 10)
+
+# Insert 0 at beginning
+a = np.insert(a, 0, 0)
+
+print(a)
+
+# =========================
+# Day 5
+# =========================
+
+# Q10.   Create array [10,20,30,40]
+#        Convert array into list
+#        फिर list को वापस array में convert करो
+
+
+import numpy as np
+a = np.array([1,2,3,4,5])
+pritn(type(a))
+b = list(a)
+print(type(b))
+b = np.array(a)
+print(type(b))
+
+
+
+# Q11.   Create array [5,10,15,20,25]
+#        Find index of value 15
+#        Replace that value with 100
+import numpy as np
+
+a = np.array([5,10,15,20,25])
+
+a[a ==15] = 100
+print(a)
+
+
+# Q12.    Create array [1,1,2,2,3,3]
+#         Remove duplicate elements
+#         Print unique array
+
+
+import numpy as np
+
+a = np.array([1,1,2,2,3,3])
+
+unique = []
+
+for i in a:
+    if i not in unique:
+        unique.append(i)
+
+# convert back to numpy array
+unique = np.array(unique)
+
+print("Unique array:", unique)
