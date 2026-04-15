f77ba374a89750e46fc1f0614dde61b0683ebeea
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-04-15 18:38:58 +0530

    Add more questions in  advanced numpy problems

--- numpy/advanced/complex_problem_2.py
+++ numpy/advanced/complex_problem_2.py
@@ -62,4 +62,20 @@ result = np.where(normalized > 0.8, 999, normalized)
 
 print("\nFinal Result:\n", result)
 
+#  Q 5.)
+a = np.array([[1,2],[3,4]])
+print(a.T)
 
+arr1 = np.random.randint(1,100,(6,6))
+print("array",arr1)
+
+c = arr1[1:5,1:5]
+print(c)
+top = c[0, :]
+bottom = c[-1, :]
+left = c[1:-1, 0]
+right = c[1:-1, -1]
+
+border_sum = top.sum() + bottom.sum() + left.sum() + right.sum()
+
+print("\nBorder Sum:", border_sum)
