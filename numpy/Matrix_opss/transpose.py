35285ce574fff246ad72d90321835e3ba78a7e22
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-04-15 18:37:21 +0530

    Add more in advanced numpy problems

--- numpy/Matrix_opss/transpose.py
+++ numpy/Matrix_opss/transpose.py
@@ -3,6 +3,19 @@ import numpy as np
 a = np.array([[1,2],[3,4]])
 print(a.T)
 
+arr1 = np.random.randint(1,100,(6,6))
+print("array",arr1)
+
+c = arr1[1:5,1:5]
+print(c)
+top = c[0, :]
+bottom = c[-1, :]
+left = c[1:-1, 0]
+right = c[1:-1, -1]
+
+border_sum = top.sum() + bottom.sum() + left.sum() + right.sum()
+
+print("\nBorder Sum:", border_sum)
 
 a = np.random.randint(1,10,(3,3))
 print(a.T)
--- numpy/advanced/complex_problem_2.py
+++ numpy/advanced/complex_problem_2.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# Q 1.)
 a = np.random.randint(1, 50, (5,5))
 
 rot = np.rot90(a, -1)
@@ -8,7 +8,7 @@ center = rot[1:4, 1:4]
 print(center)
 print("Sum:", np.sum(center))
 
-
+#  Q 2. )
 a = np.random.randint(1,100,(5,4))
 
 norm = (a - np.min(a)) / (np.max(a) - np.min(a))
@@ -30,7 +30,7 @@ print(np.diag(a))
 
 
 
-
+#  Q 3.)
 a = np.random.randint(1,100,(5,5))
 
 # Diagonal replace
@@ -40,3 +40,26 @@ np.fill_diagonal(a, 0)
 a[np.arange(5), np.arange(4,-1,-1)] = 1
 
 print(a)
+
+# Q 4. )
+
+arr = np.random.randint(1,100,(5,5))
+ 
+print("Original Array : ", arr)
+
+
+#  column-wise min & max
+col_min = arr.min(axis=0)
+col_max = arr.max(axis=0)
+
+#  Min-Max scaling
+normalized = (arr - col_min) / (col_max - col_min)
+
+print("\nNormalized Array:\n", normalized)
+
+# replace values > 0.8 with 999
+result = np.where(normalized > 0.8, 999, normalized)
+
+print("\nFinal Result:\n", result)
+
+
