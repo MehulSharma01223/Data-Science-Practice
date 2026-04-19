4c379d7ecd33d68cecb6789ff9c0f54830c819eb
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-04-19 15:56:13 +0530

    Completed Pandas groupby and aggregation with advanced analysis

--- Pandas/groupby/aggregation.py
+++ Pandas/groupby/aggregation.py
@@ -42,3 +42,41 @@ print(df.groupby("City").agg({
     "Age": ["max"]
 }))
 
+
+
+# Q7: Rename columns → avg_marks, max_marks
+
+print(df.groupby("City").agg(
+    avg_marks = ("Marks","mean"),
+    max_marks = ("Marks","max")
+))
+
+# Q8: City-wise → Marks mean + Age min + Age max (custom agg)
+
+
+print(df.groupby("City").agg({
+    "Marks" : ["mean"],
+    "Age" : ["min","max"]
+
+}))
+
+
+# Q9: Sirf Delhi & Mumbai → agg(mean, max)
+
+result = df[df["City"].isin(["Delhi","Mumbai"])]\
+            .groupby("City")["Marks"].agg(["mean","max"])
+print(result)
+
+# Q10: Har city ka topper (max Marks wala row)
+topper = df.loc[df.groupby("City")["Marks"].idxmax()]
+print(topper)
+
+# Q11: Har city me kitne students 60 se upar hai
+result = df[df["Marks"] > 60 ].groupby("City").size()
+print(result)
+# Q12: City-wise performance category banao:
+#      mean > 75 → "Good"
+#      mean > 60 → "Average"
+#      else → "Poor"
+avg = df.groupby("City")["Marks"].mean()
+
