00d1d919aa68b2f3a4ed400ba055c835decd9a10
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-04-25 10:12:00 +0530

    Day 5 Pandas: basic visualization (bar charts for city count, avg marks, top 3 students)

--- Pandas/visualization/day1_bar_chart.py
+++ Pandas/visualization/day1_bar_chart.py
@@ -19,3 +19,46 @@ plt.xlabel("City")
 plt.ylabel("Average Marks")
 
 plt.show()
+
+
+student_count = df["City"].value_counts()
+
+student_count.plot(kind="bar")
+
+plt.title("Number of Students per City")
+plt.xlabel("City")
+plt.ylabel("Number of Students")
+
+plt.show()
+
+#  Questions of bar chart 
+
+# 1. )   City-wise students count ka graph banaoge
+df.groupby("City").size().plot(kind= "bar", color = "pink")
+plt.title("Number of Students per City")
+plt.xlabel("City")
+plt.ylabel("Number of students")
+
+plt.show()
+
+# Q2 ): City-wise average marks ka graph banaoge
+df.groupby("City")["Marks"].mean().plot(kind = "bar" , color = "violet")
+plt.title("City wise average marks")
+plt.ylabel("Average number")
+plt.xlabel("City")
+plt.show()
+
+
+#  Q3 .)  top 3 students 
+top = df.sort_values("Marks" , ascending = False).head(3)
+
+top.plot(
+    x="Name",
+    y="Marks",
+    kind="bar",
+    color="skyblue"
+)
+plt.title("Top 3 Students")
+plt.ylabel("Marks")
+plt.xlabel("Students Name")
+plt.show()
