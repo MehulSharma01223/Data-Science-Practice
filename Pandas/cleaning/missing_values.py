2a60d4e167a604f430b6a5e63b4438c605200402
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-05-07 14:38:57 +0530

    feat(pandas-cleaning): add missing value detection and counting practice

--- Pandas/cleaning/missing_values.py
+++ Pandas/cleaning/missing_values.py
@@ -8,4 +8,19 @@ df = pd.DataFrame({
 })
 
 print(df)
+
+#  Check all missing values
 print(df.isnull())
+
+# Count missing values
+print(df.isnull().sum())
+
+# Check missing values only in City
+print(df["City"].isnull())
+
+# Count total missing values in dataset
+print(df.isnull().sum().sum()) 
+
+# Sirf un rows ko identify karo jaha missing value present hai.
+
+df[df.isnull().any(axis = 1)]
