53b4f9bcf9d5f6b7d9d157075803b035848d5d3b
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-06-30 11:38:06 +0530

    feat(seaborn): master histplot

---
+++ Python/seaborn/03_histplot.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+tips = sns.load_dataset("tips")
+
+plt.figure(figsize=(10,6))
+
+sns.histplot(
+    data=tips,
+    x="total_bill",
+    bins=20,
+    kde=True,
+    hue="sex",
+    palette="Set2",
+    alpha=0.6,
+    stat="count",
+    multiple="layer"
+)
+
+plt.title("Distribution of Total Bill")
+
+plt.xlabel("Total Bill ($)")
+
+plt.ylabel("Count")
+
+plt.grid(alpha=0.3)
+
+plt.tight_layout()
+
+plt.show()
+
