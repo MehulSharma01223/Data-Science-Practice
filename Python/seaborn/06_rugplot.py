c6ae38a9767fa2115685b4c53515f387b90c91a6
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-07-04 11:48:00 +0530

    feat(seaborn):  master rugplot

--- Python/seaborn/06_rugplot.py
+++ Python/seaborn/06_rugplot.py
@@ -1,20 +1,31 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_theme(style="whitegrid")
+
 iris = sns.load_dataset("iris")
-sns.histplot(
-    data =iris,
-    x = "sepal_length",
-    bins=15,
-    hue="species",
-    
+
+plt.figure(figsize=(10,6))
+
+sns.kdeplot(
+    data=iris,
+    x="sepal_length",
+    fill=True,
+    color="skyblue"
 )
 
 sns.rugplot(
     data=iris,
     x="sepal_length",
-    hue = "species",
-    color ="black"
+    hue="species",
+    height=0.06,
+    palette="Dark2",
+    alpha=0.8
 )
 
+plt.title("Sepal Length Distribution with Rug Plot")
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Density")
+
+plt.tight_layout()
 plt.show()
