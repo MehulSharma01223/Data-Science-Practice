e884f75093276885bd5441f3efbdcf85645e8fc8
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-07-04 11:36:09 +0530

    make a combine graph of kde and rugplot

--- Python/seaborn/06_rugplot.py
+++ Python/seaborn/06_rugplot.py
@@ -2,10 +2,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 iris = sns.load_dataset("iris")
+sns.kdeplot(
+    data =iris,
+    x = "sepal_length",
+    fill=True
+)
 
 sns.rugplot(
     data=iris,
-    x="sepal_length"
+    x="sepal_length",
+    color ="black"
 )
 
 plt.show()
