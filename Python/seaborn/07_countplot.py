f7f66663bd4ce6ed7a8b52b7cad9cb596d04eee0
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-07-04 12:08:18 +0530

    feat(seaborn): master countplot

--- Python/seaborn/07_countplot.py
+++ Python/seaborn/07_countplot.py
@@ -1,13 +1,24 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_theme(style="whitegrid")
+
 titanic = sns.load_dataset("titanic")
+
+plt.figure(figsize=(9,6))
 
 sns.countplot(
     data=titanic,
-    x="sex",
-    hue = "survived",
-    palette= "Set2"
+    x="class",
+    hue="survived",
+    palette="viridis"
 )
 
+plt.title("Passenger Survival by Class", fontsize=15)
+plt.xlabel("Passenger Class")
+plt.ylabel("Count")
+
+plt.grid(axis="y", alpha=0.3)
+
+plt.tight_layout()
 plt.show()
