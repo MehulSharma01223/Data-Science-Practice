37a55b604de31f904dc1c97667dac9f99ec32d0f
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-07-05 11:46:18 +0530

    feat(seaborn): master barplot

--- Python/seaborn/08_barplot.py
+++ Python/seaborn/08_barplot.py
@@ -1,16 +1,28 @@
 import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
+sns.set_theme(style="whitegrid")
 
 titanic = sns.load_dataset("titanic")
+
+plt.figure(figsize=(10,6))
 
 sns.barplot(
     data=titanic,
     x="class",
     y="fare",
     hue="sex",
-    palette="Set2",
-    estimator=np.median
+    estimator=np.mean,
+    errorbar=None,
+    palette="viridis"
 )
 
+plt.title("Average Fare by Passenger Class and Gender", fontsize=15)
+plt.xlabel("Passenger Class")
+plt.ylabel("Average Fare")
+
+plt.grid(axis="y", alpha=0.3)
+
+plt.tight_layout()
 plt.show()
