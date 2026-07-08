581bd18f9d7dedf76c6636a0a4f003541fb601c6
MehulSharma0122 <sharmamehul4649@gmail.com>
2026-07-08 12:04:17 +0530

    feat(seaborn): master violinplot

--- Python/seaborn/10_violinplot.py
+++ Python/seaborn/10_violinplot.py
@@ -1,14 +1,28 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_theme(style="whitegrid")
+
 titanic = sns.load_dataset("titanic")
+
+plt.figure(figsize=(10,6))
 
 sns.violinplot(
     data=titanic,
     x="class",
     y="fare",
     hue="sex",
-    palette="Set2"
+    split=True,
+    inner="box",
+    palette="viridis",
+    linewidth=1.2
 )
 
+plt.title("Fare Distribution by Passenger Class and Gender")
+plt.xlabel("Passenger Class")
+plt.ylabel("Fare")
+
+plt.grid(axis="y", alpha=0.3)
+
+plt.tight_layout()
 plt.show()
